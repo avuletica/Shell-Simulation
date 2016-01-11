35ab2b8003b934f9dbd14267ef5af152e394345a
Ante <vuletic23@gmail.com>
2016-01-11 18:51:55 +0100

    Change parameters of redirection_output

--- shutility.h
+++ shutility.h
@@ -3,7 +3,7 @@
 
 void display_info();
 void redirection_input(char *, char *);
-void redirection_output(char *, char *);
+void redirection_output(char *, char *, char *);
 int execute_command(char *, char *);
 void handle_pipe(char *, char *, char *, char *);
 
