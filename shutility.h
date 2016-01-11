e61e4745748a6186628742177aa9bf1587921aa4
Ante <vuletic23@gmail.com>
2016-01-11 18:11:01 +0100

    Add pipe arguments

--- shutility.h
+++ shutility.h
@@ -4,7 +4,7 @@
 void display_info();
 void redirection_input(char *, char *);
 void redirection_output(char *, char *);
-int execute_command(char*, char*);
-void handle_pipe(char*, char*);
+int execute_command(char *, char *);
+void handle_pipe(char *, char *, char *, char *);
 
 #endif
